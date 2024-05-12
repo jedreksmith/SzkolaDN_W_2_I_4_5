7847c7a39e123eacfdf5264b111cea2d00365c45
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-05-12 21:56:41 +0200

    Add project files.

---
+++ Ex_w_2_I_4_5/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ex_w_2_I_4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string firstName, secondName, phone,email;
+
+            int growth;
+            
+            double weight;
+
+            Console.WriteLine("Write your firstname");
+            firstName = (Console.ReadLine());
+
+            Console.WriteLine("Write your secondName");
+            secondName = (Console.ReadLine());
+
+            Console.WriteLine("How tall are you");
+            growth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How much you weigh");
+            weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Write your phpne  number");
+            phone = (Console.ReadLine());
+
+        }
+    }
+}
